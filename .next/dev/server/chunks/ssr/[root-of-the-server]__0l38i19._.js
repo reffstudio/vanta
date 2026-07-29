fb0afb2835a8c2084d899829dd0069aac7094f89
Eliu Benammi <eliubenammi@Elius-MacBook-Pro.local>
2026-07-28 23:02:52 -0700

    Favicon agregado

--- .next/dev/server/chunks/ssr/[root-of-the-server]__0l38i19._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__0l38i19._.js
@@ -51,20 +51,23 @@ const metadata = {
     icons: {
         icon: [
             {
-                url: '/icon-light-32x32.png',
-                media: '(prefers-color-scheme: light)'
+                url: '/favicon.ico',
+                sizes: 'any'
             },
             {
-                url: '/icon-dark-32x32.png',
-                media: '(prefers-color-scheme: dark)'
+                url: '/favicon-16x16.png',
+                sizes: '16x16',
+                type: 'image/png'
             },
             {
-                url: '/icon.svg',
-                type: 'image/svg+xml'
+                url: '/favicon-32x32.png',
+                sizes: '32x32',
+                type: 'image/png'
             }
         ],
-        apple: '/apple-icon.png'
-    }
+        apple: '/apple-touch-icon.png'
+    },
+    manifest: '/site.webmanifest'
 };
 const viewport = {
     colorScheme: 'dark',
@@ -80,18 +83,18 @@ function RootLayout({ children }) {
                 children,
                 ("TURBOPACK compile-time value", "development") === 'production' && /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f40$vercel$2f$analytics$2f$dist$2f$next$2f$index$2e$mjs__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["Analytics"], {}, void 0, false, {
                     fileName: "[project]/app/layout.tsx",
-                    lineNumber: 46,
+                    lineNumber: 38,
                     columnNumber: 51
                 }, this)
             ]
         }, void 0, true, {
             fileName: "[project]/app/layout.tsx",
-            lineNumber: 44,
+            lineNumber: 36,
             columnNumber: 7
         }, this)
     }, void 0, false, {
         fileName: "[project]/app/layout.tsx",
-        lineNumber: 43,
+        lineNumber: 35,
         columnNumber: 5
     }, this);
 }
